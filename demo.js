30ac598d5ba81e2fd654c7b11386043db3a1a150
iqrakhann <160530247+iqrakhann@users.noreply.github.com>
2024-03-18 17:32:14 +0500

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,42 @@
+// **OBJECT OFF TYPESCRIPT**
+var personinfo = {
+    Name: "iqra",
+    lastName: "khan",
+    age: 27,
+    email: "sk7009001@gmail.com",
+    phonNumber: "03173308472",
+    roolNumber: "00244539",
+    address: "mehmoodabad number 4",
+    street: "10 A",
+    city: "karachi",
+    country: "pakistan",
+    zipCode: 75460
+};
+console.log(personinfo);
+// ** EXAMPLE** #1
+var car = {
+    carName: "civic",
+    carColour: "white",
+    carMaker: "Honda",
+    carBrand: "corolla",
+    year: 2018,
+};
+console.log(car);
+// ** EXAMPLE ** #2
+var fridge = {
+    name: "Haier",
+    colour: "silver",
+    model: "368.EBS",
+    maker: "made in pakistan",
+    QApassed: true,
+};
+console.log(fridge);
+// ** EXEMPLE ** #3
+var bikedetails = {
+    bikeName: "kawasaki",
+    bikeColour: "red",
+    bikeimport: "japan",
+    bikecc: "125",
+    year: 1990
+};
+console.log(bikedetails);
---
+++ demo.ts
@@ -0,0 +1,73 @@
+
+
+// **OBJECT OFF TYPESCRIPT**
+
+let personinfo = {
+    Name:"iqra",
+    lastName:"khan",
+    age:27,
+    email:"sk7009001@gmail.com",
+    phonNumber:"03173308472",
+    roolNumber:"00244539",
+    address:"mehmoodabad number 4",
+    street:"10 A",
+    city:"karachi",
+    country:"pakistan",
+    zipCode:75460
+    
+     
+}
+
+console.log(personinfo);
+
+// ** EXAMPLE** #1
+
+let car:  {
+    carName: string,
+    carColour: string,
+    carMaker: string,
+    carBrand: string,
+    year: number,
+}={
+    carName: "civic",
+    carColour: "white",
+    carMaker: "Honda",
+    carBrand: "corolla",
+    year: 2018,
+}
+console.log(car)
+
+// ** EXAMPLE ** #2
+let fridge: {
+    name:string,
+    colour:string,
+    model:string,
+    maker:string,
+    QApassed: boolean,
+}={
+    name: "Haier",
+    colour: "silver",
+    model: "368.EBS",
+    maker: "made in pakistan",
+    QApassed: true,
+}
+
+console.log(fridge);
+
+// ** EXEMPLE ** #3
+let bikedetails: {
+    bikeName: string,
+    bikeColour: string,
+    bikeimport: string,
+    bikecc: string,
+    year: number,
+}={
+    bikeName:"kawasaki",
+    bikeColour:"red",
+    bikeimport:"japan",
+    bikecc: "125",
+    year:1990
+
+}
+
+console.log(bikedetails)
