19246651e62fa95b977976b9c0c41396cdcdfcf7
iqra tariq <160530247+iqrakhann@users.noreply.github.com>
2024-03-31 00:51:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+// SIMPLE FUNCTION #01
+function multipleValue() {
+    var answer1 = 5 + 9;
+    var answer2 = 5 - 9;
+    var answer3 = 5 * 9;
+    var answer4 = 5 / 9;
+    console.log(answer1, answer2, answer3, answer4);
+}
+multipleValue();
+//FUNCITION  #RETURN# 02
+function carRent() {
+    var carRent = 5000 * 7;
+    console.log(carRent);
+    return carRent;
+}
+carRent();
+//FUNCITION #03
+// rice 5,oil 4,salt 2, 
+function makeBiryani() {
+    var cooked = 5 + 4.1 + 2;
+    console.log(cooked);
+    return cooked;
+}
+makeBiryani();
+//FUNCTION #04
+function guest() {
+    var name = "iqra tariq";
+    console.log(name);
+}
+guest();
+function guest1() {
+    var name1 = "Maham";
+    var name2 = "usman";
+    var name3 = "zeeshan";
+    var name4 = "Rehan";
+    var name5 = "Hamdan";
+    console.log(name1, name2, name3, name4, name5);
+}
+guest1();
+function guest2() {
+    var name1 = "Minahil";
+    var name2 = "Horiya";
+    var name3 = "Ayeza";
+    var name4 = "Arshuman";
+    var name5 = "Ariba";
+    console.log(name1, name2, name3, name4, name5);
+}
+guest2();
+// FUNCITION #05
+function sume() {
+    var number = 10 + 12 - 30 * 10 / 40;
+    console.log(number);
+}
+sume();
---
+++ app.ts
@@ -0,0 +1,70 @@
+
+// SIMPLE FUNCTION #01
+
+function multipleValue(): void{
+    let answer1 = 5+9
+    let answer2 = 5-9
+    let answer3 = 5*9
+    let answer4 = 5/9
+    console.log(answer1,answer2,answer3,answer4)
+}    
+
+multipleValue()
+
+//FUNCITION  #RETURN# 02
+
+function carRent(): number {
+    let carRent = 5000*7;
+    console.log(carRent);
+    return carRent
+}
+carRent();
+
+//FUNCITION #03
+
+// rice 5,oil 4,salt 2, 
+
+function makeBiryani(): number{  
+    let cooked=5+4.1+2
+    console.log(cooked)
+    return cooked  
+}
+    makeBiryani()
+
+//FUNCTION #04
+
+function guest(): void{
+    let name= "iqra tariq"
+    console.log(name);
+}
+
+guest();
+
+function guest1(): void{
+    let name1= "Maham"
+    let name2= "usman"
+    let name3= "zeeshan"
+    let name4= "Rehan"
+    let name5= "Hamdan"
+    console.log(name1,name2,name3,name4,name5);
+}
+guest1();
+
+function guest2(): void{
+    let name1= "Minahil"
+    let name2= "Horiya"
+    let name3= "Ayeza"
+    let name4= "Arshuman"
+    let name5= "Ariba"
+    console.log(name1,name2,name3,name4,name5)
+}
+guest2();
+
+// FUNCITION #05
+
+function sume(): void{
+    let number=10+12-30*10/40
+    console.log(number);
+    
+}
+sume()
